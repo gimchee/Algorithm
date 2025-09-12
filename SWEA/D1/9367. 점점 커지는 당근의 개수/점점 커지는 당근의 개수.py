4492bd26ea8e40598b91f44a92602a9e59b84bb9
gimchee <rlaclgns123@naver.com>
2025-09-12 14:32:14 +0900

    [D1] Title: 점점 커지는 당근의 개수, Time: 66 ms, Memory: 55,936 KB -BaekjoonHub

--- SWEA/D1/9367. 점점 커지는 당근의 개수/점점 커지는 당근의 개수.py
+++ SWEA/D1/9367. 점점 커지는 당근의 개수/점점 커지는 당근의 개수.py
@@ -1,28 +1,25 @@
-# 2025-09-06 
-# SWEA 9367. 점점 커지는 당근의 개수
+# 2025-09-12
+# SWEA 9367 점점 커지는 당근의 개수
 
 
 
 T = int(input())
 
-for test_case in range(1, T+1):
-
+for tc in range(1, T+1):
     N = int(input())
 
     C = list(map(int, input().split()))
 
     count = 1
-    temp = 0
     max_count = 1
-    for i in range(1,N):
 
-        if C[i-1] < C[i]:
-
+    for i in range(N-1):
+        if C[i] < C[i+1]:
             count += 1
-   
         else:
-
             count = 1
 
-        max_count = max(count, max_count)
-    print(f'#{test_case} {max_count}')
+        max_count = max(max_count,count)
+
+    print(f'#{tc} {max_count}')        
+
